89bdb5d52aebfa2f942b1b8a8593089ae4a96f3a
Arvind Srinivasan <mail@arvind.one>
2025-11-24 14:09:43 +0100

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'snake-game-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'music.m4a',
+  'icon-512.png'
+];
+
+// Install service worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache.map(url => new Request(url, { cache: 'reload' })))
+          .catch(err => {
+            console.log('Cache addAll error:', err);
+            // Continue even if some files fail to cache
+            return Promise.resolve();
+          });
+      })
+  );
+  self.skipWaiting();
+});
+
+// Fetch from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate service worker and remove old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
